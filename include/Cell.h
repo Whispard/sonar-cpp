2916927cc70f3a305ac8880c93faa8e7cda7fe79
Whispard <jayyadav1863@gmail.com>
2021-12-10 21:15:38 +0530

    base ready

--- include/Cell.h
+++ include/Cell.h
@@ -12,12 +12,15 @@ enum class CellType {
 
 // Cell Kind & Display
 struct Cell {
+    Cell() = default;
     CellType kind = CellType::Empty;
     // TODO: Decouple this distance filed from here
     int distance = 0;
 
+
     void showRange(int d) ;
 
     std::string getDrawing(int randomNum) const;
+//    virtual ~Cell() = 0;
 };
 
